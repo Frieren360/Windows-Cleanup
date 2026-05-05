887be1e5140695cf6ba7ddeedbefe834d95612c2
Frieren360 <162036597+Frieren360@users.noreply.github.com>
2026-05-05 13:51:31 +0100

    Replace psexec with PowerShell Invoke-Command

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,6 @@
 
 call config.bat
 
-psexec @pcs.txt -s -d cmd /c "%SOURCE%"
+for /f "delims=" %%A in pcs.txt do (
+  Invoke-Command -ComputerName %%A -ScriptBlock { cmd.exe /c "\\192.168.5.253\storage\cleanup-script\clean-fixed.bat" }
+)
